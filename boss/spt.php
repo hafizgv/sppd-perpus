85e2006b29388a2a090d1ec99c2b7c7365a4a4e5
HafizGv <hafizgv@gmail.com>
2023-12-13 17:21:18 +0700

    Pemimpin Page v.1

--- boss/spt.php
+++ boss/spt.php
@@ -4,13 +4,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../output.css" rel="stylesheet">
-        <title>Gw Boss</title>
+        <title>LPD Boss</title>
     </head>
     <body>
         <section class="h-screen flex">
             <?php include '../components/sidebar_p.php' ?>
-            <div class="p-6 bg-red-300 w-screen">
-                <h2>Muehehe</h2>
+            <div class="p-6 bg-emerald-300 w-screen">
+                <h2>Oh, jadi ini suratnya</h2>
             </div>
         </section>
     </body>
