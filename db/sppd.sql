c3aaa853bba74a36d7252c27021eb126f8b7d29d
HafizGv <hafizgv@gmail.com>
2023-12-24 09:26:52 +0700

    new db

--- db/sppd.sql
+++ db/sppd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2023 at 09:12 AM
+-- Generation Time: Dec 24, 2023 at 03:25 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -43,9 +43,12 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id_user`, `username`, `password`, `tier`, `nip`, `nama`, `pangkat`, `golongan`) VALUES
-(1, 'boss', 'boss', 'Pimpinan', '1992588512019212', 'Bobi Sinagartulo', 'Pembina', 'Golongan IV'),
-(3, 'bidum', 'bidum', 'Bidum', '1992588512019513', 'Andi Lukito', 'Bengkok satu', 'Menengah kebawah'),
-(5, 'kabid', 'kabid', 'Kabid', '1992555212019213', 'Kleopatra', 'Diamond satu', 'Menengah keatas');
+(3, 'bidum', 'bidum', 'Bidum', '1992588512019513', 'Andi Lukito', 'penata muda', 'IIb'),
+(5, 'kabid', 'kabid', 'Kabid', '19925552120', 'Kleopatra', 'pembina', 'IIId'),
+(6, 'bidum', 'cut123', 'Pranata Teknologi ', '11129345699', 'cut mardiah', 'penata tingkat 1', 'IIId'),
+(7, 'feby', 'feby123', 'Pranata Teknologi ', '2331115676543', 'feby anggreani', 'penata muda', 'IIIc'),
+(8, 'jimy', 'jimy123', 'bidum', '3587891763211', 'jimmy triandana', 'penata tingkat 1', 'IIb'),
+(9, 'nova', 'nova12', 'Pimpinan', '88974347771442', 'nova fadillah', 'pembina', 'IVa');
 
 -- --------------------------------------------------------
 
@@ -66,8 +69,8 @@ CREATE TABLE `lpd` (
 --
 
 INSERT INTO `lpd` (`no_spd`, `petugas`, `filename`, `hasil`, `tgl_upload`) VALUES
-('SPD/200/B', 'Hafiz', 'Hafiz_200504016.pdf', 'ada KRS didalamnya!!!', '2023-12-15'),
-('SPD/200/D', 'Bobi', 'Bedess.jpeg', 'akuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', '2023-12-15'),
+('spd/001/L', 'feby anggreani', 'download.jpg', 'berjalan dengan lancar', '2023-12-20'),
+('spd/002', 'jimmy triandana', 'download.jpg', 'ada kendala', '2023-12-20'),
 ('SPD/200/L', 'Andi Lukito', 'crz.jpg', 'wew', '2023-12-15');
 
 -- --------------------------------------------------------
@@ -79,21 +82,25 @@ INSERT INTO `lpd` (`no_spd`, `petugas`, `filename`, `hasil`, `tgl_upload`) VALUE
 CREATE TABLE `spd` (
   `no_spd` varchar(50) NOT NULL,
   `petugas` varchar(50) NOT NULL,
-  `tugas` text NOT NULL,
-  `tujuan` varchar(100) NOT NULL,
+  `perihal` text NOT NULL,
+  `lokasi` varchar(100) NOT NULL,
   `verify` int(1) NOT NULL,
-  `filename` varchar(100) NOT NULL
+  `filename` varchar(100) NOT NULL,
+  `tujuan` varchar(100) NOT NULL,
+  `asal` varchar(100) NOT NULL,
+  `dateIn` date NOT NULL,
+  `dateOut` date NOT NULL,
+  `dateSend` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `spd`
 --
 
-INSERT INTO `spd` (`no_spd`, `petugas`, `tugas`, `tujuan`, `verify`, `filename`) VALUES
-('SPD/200/B', 'Hafiz', 'Mencari Ikan kakap', 'Ngawi', 1, 'sign.png'),
-('SPD/200/D', 'Bobi', 'Nyangkol tanah', 'Lahan tetangga', 0, '52xhqg.jpg'),
-('SPD/200/F', 'Kevin Anggara', 'Menjaga Ayam', 'Ga kecolongan', 0, 'data-diri.jpg'),
-('SPD/200/L', 'Andi Lukito', 'Jadi Alien ', 'Menginvasi bumi', 2, 'data-diri.jpg');
+INSERT INTO `spd` (`no_spd`, `petugas`, `perihal`, `lokasi`, `verify`, `filename`, `tujuan`, `asal`, `dateIn`, `dateOut`, `dateSend`) VALUES
+('SPD/001/A', 'Andi Lukito', 'Makan Ayam Bakar', 'Rumah', 1, '2ee2.png', 'Kuliner', 'Ayam Penyet Pak Ulis', '2024-01-04', '2024-01-05', '2023-12-23'),
+('SPD/002/A', 'Hafiz', 'Bertani sawah', 'Rumah Pak RT', 0, '52xhqg.jpg', 'Kultivasi', 'PT. Jaya Sejahtera', '2024-01-01', '2024-01-06', '2023-12-23'),
+('SPD/003/A', 'Bobi Siregar', 'Menjaga kandang', 'Belakang Rumah', 2, '52xhqg.jpg', 'Peternakan', 'Sapi Pak Ben', '2024-01-04', '2024-01-06', '2023-12-23');
 
 --
 -- Indexes for dumped tables
@@ -125,7 +132,7 @@ ALTER TABLE `spd`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
